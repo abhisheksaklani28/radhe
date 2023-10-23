8bbbccfaa13dbe7efd88411fcae5aa05694defc1
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-10-23 10:08:10 +0530

    commit fiorst time

---
+++ ttt/src/mmm/nnn.java
@@ -0,0 +1,17 @@
+package mmm;
+
+public class nnn 
+{
+	 int a;
+	public void m1()
+	{
+		System.out.println("what is this ");
+	}
+public static void main(String[] args)
+{
+	nnn obj=new nnn();
+	int a=obj.a=125;
+System.out.println("the vale of a "+a);
+	
+}
+}
